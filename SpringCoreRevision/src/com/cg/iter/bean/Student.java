6bc5e761f00881793c5740cd97c5a7455a56a1c6
karanj15 <karan.ksj15@gmail.com>
2020-04-28 09:05:28 +0530

    first commit

---
+++ SpringCoreRevision/src/com/cg/iter/bean/Student.java
@@ -0,0 +1,114 @@
+package com.cg.iter.bean;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+
+@Entity
+public class Student {
+	@Id
+	private int id;
+	private String name;
+	private long mobile;
+	private String subject;
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="addr_id")
+	private Address address;
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Student(int id, String name, long mobile, String subject) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mobile = mobile;
+		this.subject = subject;
+	}
+	
+	public Student(int id, String name, long mobile, String subject, Address address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mobile = mobile;
+		this.subject = subject;
+		this.address = address;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", mobile=" + mobile + ", subject=" + subject + "]";
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the mobile
+	 */
+	public long getMobile() {
+		return mobile;
+	}
+	/**
+	 * @param mobile the mobile to set
+	 */
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+	/**
+	 * @return the subject
+	 */
+	public String getSubject() {
+		return subject;
+	}
+	/**
+	 * @param subject the subject to set
+	 */
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	/**
+	 * @return the address
+	 */
+	public Address getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	@Autowired
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+
+	
+	
+}
